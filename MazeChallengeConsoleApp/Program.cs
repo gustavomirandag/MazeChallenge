e705578f068a7a3de6b2691a6b31395681d87fbe
gustavomirandag <gustavomirandag@gmail.com>
2019-08-02 16:21:02 -0300

    Fixed the Maze Generator.

--- MazeChallengeConsoleApp/Program.cs
+++ MazeChallengeConsoleApp/Program.cs
@@ -6,34 +6,56 @@ namespace MazeChallengeConsoleApp
     {
         static void Main(string[] args)
         {
-            char[,] matrix = GenerateMatrix(20, 100);
-            PrintMatrix(matrix);
+            char[,] maze = GenerateMaze(20, 100);
+            FindExitPath(maze);
+            PrintMaze(maze);
 
             Console.ReadKey();
         }
 
-        static void FindExitPath(char[,] matrix, int row, int col)
+        static void FindExitPath(char[,] maze)
         {
             //IMPLEMENTAR A SOLUÇÃO PARA O DESAFIO AQUI!
             //Implemente uma solução que encontre a saída do labirinto
 
             //PS1: Não precisa ser o caminho mais curto, apenas uma saída.
             //PS2: Pintar o caminho da saida com algum caractere. Ex: '*'
+
+            //First Step: Find the ENTRANCE.
+            int startCol = 0;
+            for (int i = 1; i < maze.GetLength(1); i++)
+                if (maze[0, i] == ' ')
+                {
+                    startCol = i;
+                    break;
+                }
+
+            //Second Step: FindTheExit
+            RecursivelyFindExitPath(maze, 0, startCol);
         }
 
-        static void PrintMatrix(char[,] matrix)
+        private static bool RecursivelyFindExitPath(char[,] maze, int row, int col)
         {
-            for (int row = 0; row < matrix.GetLength(0); row++)
+            //Se quiser trabalhar de forma recursiva, basta preencher esta função!!!!
+        }
+
+        static void PrintMaze(char[,] maze)
+        {
+            for (int row = 0; row < maze.GetLength(0); row++)
             {
-                for (int col = 0; col < matrix.GetLength(1); col++)
+                for (int col = 0; col < maze.GetLength(1); col++)
                 {
-                    Console.Write(matrix[row, col] + "");
+                    if (maze[row, col] == '*')
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                    else
+                        Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write(maze[row, col]);
                 }
                 Console.WriteLine();
             }
         }
 
-        static char[,] GenerateMatrix(int rowSize, int colSize)
+        static char[,] GenerateMaze(int rowSize, int colSize)
         {
             char[,] matrix = new char[rowSize, colSize];
 
@@ -66,8 +88,8 @@ namespace MazeChallengeConsoleApp
                 Console.WriteLine();
             }
 
-            //Generate the Exit
-            int startRow = 0; //The entrance
+            //Generate the Entrance and the Exit
+            int startRow = 0; //Entrance row
             int startCol = rand.Next(1, colSize - 1);
             matrix[startRow, startCol] = ' ';
             startRow++;
@@ -76,33 +98,35 @@ namespace MazeChallengeConsoleApp
             return matrix;
         }
 
-        static void GeneratePath(Random rand, char[,] matrix, int row, int col)
+        static void GeneratePath(Random rand, char[,] maze, int row, int col)
         {
-            while (row < matrix.GetLength(0))
+            while (row < maze.GetLength(0))
             {
-                matrix[row, col] = ' ';
+                maze[row, col] = ' ';
 
-                if (row == matrix.GetLength(0) - 1)
+                if (row == maze.GetLength(0) - 1)
                     return;
 
                 int randResult = rand.Next(300);
 
                 if (randResult > 160)
                 {
-                    col = rand.Next(col, matrix.GetLength(1) - 1);
-                    for (int curCol = col; curCol <= col; curCol++)
-                        matrix[row, curCol] = ' ';
+                    int nextCol = rand.Next(col, maze.GetLength(1) - 1);
+                    for (; col <= nextCol; col++)
+                        maze[row, col] = ' ';
+                    col--;
                 }
                 else if (randResult >= 10 && randResult <= 160)
                 {
-                    col = rand.Next(1, col);
-                    for (int curCol = col; curCol < col; curCol++)
-                        matrix[row, curCol] = ' ';
+                    int nextCol = rand.Next(1, col);
+                    for (; col >= nextCol; col--)
+                        maze[row, col] = ' ';
+                    col++;
                 }
 
                 row++;
-                if (row < matrix.GetLength(0))
-                    matrix[row, col] = ' ';
+                if (row < maze.GetLength(0))
+                    maze[row, col] = ' ';
                 row++;
             }
         }
